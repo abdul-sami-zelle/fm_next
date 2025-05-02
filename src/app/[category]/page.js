edaa589f33f4738bf4c6391037435a4caa174137
Rashid-zellesolutions <rashidali.zellesolutions@gmail.com>
2025-05-02 11:29:00 +0500

    SEO Set For Category page

--- src/app/[category]/page.js
+++ src/app/[category]/page.js
@@ -1,25 +1,18 @@
 import CategoriesClient from "@/UI/Components/CategoryClient/CategoryClient";
 
-// export function generateMetadata({ params }) {
-//   return {
-//     title: `Category - ${params.category}`,
-//     description: `Browse our ${params.category} collection`,
-//   };
-// }
+export function generateMetadata({ params }) {
+  return {
+    title: `Category - ${params.category}`,
+    description: `Browse our ${params.category} collection`,
+  };
+}
 
 
 
 
 export default function Category({ params }) {
-  // return <CategoriesClient category={params.category} />;
   return <CategoriesClient category={params.category} />
 }
 
 
 
-// export async function generateMetadata({ params }) {
-//   return {
-//     title: `Product - ${params} `,
-//     description: `Details about `,
-//   };
-// }
